eaca4cfcae5e708230cf4c3cbaf86d1436ea7f89
SmetaninaA.E <portmanenatliene@mail.ru>
2024-03-25 00:10:12 +0300

    Revert "Delete program/multimedia_and_user_interface.py"
    This reverts commit c64db29f08511432206c55008429bfe708d29f53.

---
+++ program/multimedia_and_user_interface.py
@@ -0,0 +1,65 @@
+# Ветка 2: Мультимедиа и пользовательский интерфейс
+
+# Модуль Анимации
+class Animation:
+    def __init__(self):
+        pass
+
+    def animate_movement(self):
+        # код анимации движения фигур и других событий
+        pass
+
+# Модуль Музыки и Звуков
+class Audio:
+    def __init__(self):
+        pass
+
+    def play_sound(self, sound):
+        # код воспроизведения аудиоэффектов и музыки
+        pass
+
+# Модуль Меню
+class Menu:
+    def __init__(self):
+        pass
+
+    def display_main_menu(self):
+        # код отображения главного меню
+        pass
+
+    def display_settings_menu(self):
+        # код отображения меню настроек
+        pass
+
+    # и так далее для других меню
+
+# Модуль Чата
+class Chat:
+    def __init__(self):
+        pass
+
+    def send_message(self, message):
+        # код отправки сообщения
+        pass
+
+    def receive_message(self):
+        # код получения сообщения
+        pass
+
+# Модуль Системных Сообщений
+class SystemMessages:
+    def __init__(self):
+        pass
+
+    def display_message(self, message):
+        # код отображения системных сообщений
+        pass
+
+# Модуль Подсказок и Инструкций
+class HintsAndInstructions:
+    def __init__(self):
+        pass
+
+    def display_hint(self, hint):
+        # код отображения подсказок и инструкций
+        pass
